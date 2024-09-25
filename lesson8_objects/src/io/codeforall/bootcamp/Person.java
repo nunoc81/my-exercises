a3a50e74ebe6fc1a9f4973e6c871b304493fa7c0
nunoc81 <ncodeforall@gmail.com>
2024-09-25 18:14:42 +0100

    Add files via upload

---
+++ lesson8_objects/src/io/codeforall/bootcamp/Person.java
@@ -0,0 +1,20 @@
+package io.codeforall.bootcamp;
+
+public class Person {
+
+    //state of object
+    public int age;
+    public String name;
+
+    public Person (int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+        public Person(){
+
+        }
+
+
+
+
+}
---
+++ lesson8_objects/src/io/codeforall/bootcamp/Sandbox.java
@@ -0,0 +1,9 @@
+package io.codeforall.bootcamp;
+
+public class Sandbox {
+    public static void main(String[] args) {
+
+        Person person1 = new Person (20,"Nuno");
+        System.out.println( person1.name + " age is " + person1.age);
+    }
+}
