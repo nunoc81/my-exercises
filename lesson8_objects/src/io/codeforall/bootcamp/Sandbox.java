2131763bc728ad299d0c2d1cac6ed286d1850b95
nunoc81 <ncodeforall@gmail.com>
2024-10-15 17:42:43 +0100

    Delete lesson8_objects directory

--- lesson8_objects/src/io/codeforall/bootcamp/Sandbox.java
+++
@@ -1,9 +0,0 @@
-package io.codeforall.bootcamp;
-
-public class Sandbox {
-    public static void main(String[] args) {
-
-        Person person1 = new Person (20,"Nuno");
-        System.out.println( person1.name + " age is " + person1.age);
-    }
-}
